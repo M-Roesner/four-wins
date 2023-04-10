91edd2b4768ac2395447b50606726a1e01e9e7b5
M-Roesner <markus_roesner@web.de>
2023-04-10 19:28:37 +0200

    add responsive design

--- index.php
+++ index.php
@@ -5,22 +5,26 @@
 ?>
 <body>
     <div class="grid_box">
-        <div class="grid_left">
-
-        </div>
-        <div class="grid_right">
-            <?php // 'grid_right' direct after 'grid_left', because background-image Position ?>
-        </div>
-
+        <div class="grid_site_generals grid_left <?= ($isReset) ? "grid_left_initial" : ""?> <?= ($activePlayer == 1) ? "grid_left_active" : ""?>"></div>
+        <div class="grid_site_generals grid_right <?= ($isReset) ? "grid_right_initial" : ""?> <?= ($activePlayer == 2) ? "grid_right_active" : ""?>"></div>
+        
         <div class="grid_versus">
-            <div class="player_red <?= ($activePlayer == 1) ? "active" : "" ?>">
-                <img src="./public/img/red.png" alt="player red" class="vs_icon">
+            <div class="versus_generals player_vs">
+                <img src="./public/img/VS.png" alt="vs" class="versus_icons icon_vs">
             </div>
-            <div class="player_vs">
-                <!-- <p>V.S.</p> -->
+            <div class="versus_generals player_red">
+                <?php
+                    if($activePlayer == 1)
+                        echo "<img src='./public/img/poow.png' alt='poow' class='icon_poow'>";
+                    ?>               
+                <img src="./public/img/tokenRed.png" alt="player red" class="versus_icons">
             </div>
-            <div class="player_blue <?= ($activePlayer == 2) ? "active" : "" ?>">
-                <img src="./public/img/blue.png" alt="player blue" class="vs_icon">
+            <div class="versus_generals player_blue">
+                <?php 
+                if($activePlayer == 2) 
+                    echo "<img src='./public/img/poow.png' alt='poow' class='icon_poow'>";
+                ?>
+                <img src="./public/img/tokenBlue.png" alt="player blue" class="versus_icons">
             </div>
         </div>
         
@@ -29,7 +33,7 @@
                 <?php
                     if($pf->hasWon($oldPlayer) && $oldPlayer != 0){
                 ?>
-                    <div class="display_win">
+                    <div class="playground_display_win">
                         <a href="index.php?player=<?= $oldPlayer ?>&reset=true">
                             <img src="<?= ($oldPlayer == 1) ? "./public/img/PlayerOneWins.png" : "./public/img/PlayerTwoWins.png" ?>" alt="Spieler gewinnt" class="win_icon">
                         </a>
@@ -47,32 +51,48 @@
 
                     echo "<table>";
                     for ($i = 0; $i < $rows + 1; $i++) {
-                    echo "<tr>";
-                    if ($i == 0) {
-                        for ($j = 0; $j < $cols; $j++) {
-                            echo "<th class=''>";
-                            echo "<input type='submit' name='btn_col' value='Row " . $j + 1 . "' class='btn_imput'>";
-                            echo "</th>";
+                        echo "<tr>";
+                        if ($i == 0) {
+                            for ($j = 0; $j < $cols; $j++) {
+                                echo "<th>";
+                                    echo "<input type='submit' name='btn_col' value='r " . $j + 1 . "' class='btn_imput'>";
+                                echo "</th>";
+                            }
+                        } else{
+                            for ($j = 0; $j < $cols; $j++) {
+                                $playerpath = match ($pf->getCoin($i - 1, $j)) {
+                                    "1" => "./public/img/tokenRed.png",
+                                    "2" => "./public/img/tokenBlue.png",
+                                    default => "./public/img/token.png",
+                                };
+                                echo "<td class='token_generals token_player0" . $pf->getCoin($i - 1, $j) . "'>";
+                                    echo "<img src='" . $playerpath . "' alt='player' class='tokens'>";
+                                echo "</td>";
+                            }
+                            }
+                        echo "</tr>";
                         }
-                    } else{
-                        for ($j = 0; $j < $cols; $j++) {
-                            echo "<td class='token_generals token_player" . $pf->getCoin($i - 1, $j) . "'>";
-                            echo "</td>";
-                        }
-                        }
-                    echo "</tr>";
-                    }
                     echo "</table>";
                 ?>
                 
             </form>
         </div>
-
-        <div class="grid_button">
+        
+        <div class="grid_reset_button">
             <form action="index.php" method="get">
                 <input type="hidden" name="reset" value="true">
-                <input class="reset" type="submit" value="New Party!">
+                <input class="btn_reset" type="submit" value="New Party!">
             </form>
+        </div>
+
+        <div class="grid_footer">
+            <p>createy by</p> 
+            <a href="https://github.com/S-Hepting" target="_blank"> S. Hepting </a>
+            <p>and</p>
+            <a href="https://github.com/M-Roesner" target="_blank"> M. Rösner </a>
+            <p>, used</p>
+            <a href="index.php" target="_blank" class="impressum"> sources</a>
+            <p>!</p>              
         </div>
     </div>
     
--- private/classes/session.php
+++ private/classes/session.php
@@ -1,19 +1,24 @@
 <?php
 require_once ('./private/classes/Playerground.php');
 
-if (session_status() == PHP_SESSION_NONE) {
+if(session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-if (isset($_GET['reset'])) {
+$isReset = true;
+if(isset($_GET['reset'])) {
     session_unset();
+} else {
+    $isReset = false;
 }
+
 
 $pf = new Playground(7,6);
 $startedPlayer = 0; // player who begun
 $activePlayer = 0;
 $oldPlayer = 0; // Übergabe über GET-Methode
 $column; // Übergabe über GET-Methode
+
 
 // rolls who begins the match, else sets params and makeMove
 if (!isset($_GET['startedPlayer']) ) {
@@ -23,7 +28,7 @@ if (!isset($_GET['startedPlayer']) ) {
     $startedPlayer = $_GET['startedPlayer'];
     if (isset($_GET['btn_col']) && isset($_GET['player'])) {
         $column = $_GET['btn_col'];
-        $column = substr($column,4,strlen($column)) - 1; // remove 'Reihe ' from button value
+        $column = substr($column,2,strlen($column)) - 1; // remove 'Reihe ' from button value
         $oldPlayer = $_GET['player'];
     
         $activePlayer = $pf->makeMove($column, $oldPlayer, $startedPlayer);
